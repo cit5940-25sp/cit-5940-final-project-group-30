f4826f69db56b336b1e459ce01f54010cfeea61a
alinigui <alinigui@seas.upenn.edu>
2025-04-07 21:24:02 -0400

    Create GameView.java

---
+++ src/GameView.java
@@ -0,0 +1,15 @@
+public class GameView {
+    private TextUI textUI;
+
+    public void displayBoard(GameState state) {
+        // TODO: Implement board display logic
+    }
+
+    public void showHistory(GameState state) {
+        // TODO: Implement move history display
+    }
+
+    public void announceWinner(Player player) {
+        // TODO: Implement winner announcement
+    }
+}
