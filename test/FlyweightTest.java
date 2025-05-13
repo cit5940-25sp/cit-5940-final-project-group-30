b7e59f9067587a5eb817454527576273e3a5c074
alinigui <alinigui@seas.upenn.edu>
2025-05-12 20:25:16 -0400

    Update FlyweightTest.java

--- test/FlyweightTest.java
+++ test/FlyweightTest.java
@@ -1,19 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FlyweightTest {
-
     @Test
-    public void sameNameAndRoleYieldsSameInstance() {
-        var p1 = MovieFlyweight.getPerson("Tom Hanks", "actor");
-        var p2 = MovieFlyweight.getPerson("Tom Hanks", "actor");
-        assertSame(p1, p2, "Flyweight must return the same instance for identical name+role");
+    void testGetPerson() {
+        MovieFlyweight.Person p1 = MovieFlyweight.getPerson("Tom Hanks", "actor");
+        MovieFlyweight.Person p2 = MovieFlyweight.getPerson("Tom Hanks", "actor");
+        MovieFlyweight.Person p3 = MovieFlyweight.getPerson("Tom Hanks", "director");
+        MovieFlyweight.Person p4 = MovieFlyweight.getPerson("Steven Spielberg", "director");
+
+        assertSame(p1, p2);
+        assertNotSame(p1, p3);
+        assertNotSame(p1, p4);
+        assertNotSame(p3, p4);
     }
 
     @Test
-    public void differentRoleYieldsDifferentInstance() {
-        var a1 = MovieFlyweight.getPerson("John Williams", "composer");
-        var a2 = MovieFlyweight.getPerson("John Williams", "director");
-        assertNotSame(a1, a2, "Same name but different role should produce distinct instances");
+    void testPersonEquality() {
+        MovieFlyweight.Person p1 = MovieFlyweight.getPerson("Tom Hanks", "actor");
+        MovieFlyweight.Person p2 = MovieFlyweight.getPerson("tom hanks", "ACTOR");
+        MovieFlyweight.Person p3 = MovieFlyweight.getPerson("Tom Hanks", "director");
+
+        assertEquals(p1, p2);
+        assertNotEquals(p1, p3);
+    }
+
+    @Test
+    void testPersonProperties() {
+        MovieFlyweight.Person p = MovieFlyweight.getPerson("Quentin Tarantino", "director");
+        assertEquals("Quentin Tarantino", p.getName());
+        assertEquals("director", p.getRole());
     }
 }
